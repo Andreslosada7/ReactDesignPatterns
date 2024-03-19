8f5072a1f098ff10948619e3a6c6108000f65c75
Andrés Losada <andreslosada7@gmail.com>
2024-03-19 08:35:49 -0500

    Feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>React Design Patterns</h1>
+
+      <p>
+        This is a guide on how we can use React design patterns to improve our
+        code.
+      </p>
+    </>
+  );
+}
+
+export default App;
